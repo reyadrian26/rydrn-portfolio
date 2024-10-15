dbfc25ac9ba785fc7a46e477be6f0504e5351cae
Rey Adrian Inday <72378242+reyadrian26@users.noreply.github.com>
2024-10-15 15:21:39 +0800

    update resume file path

--- Components/About/About.jsx
+++ Components/About/About.jsx
@@ -26,7 +26,7 @@ const About = () => {
             Contact Me
           </a>
           <a
-            href="/files/RESUME - REY ADRIAN INDAY.pdf"
+            href="../../public/files/RESUME - REY ADRIAN INDAY.pdf"
             download
             className={styles.btn}
           >
